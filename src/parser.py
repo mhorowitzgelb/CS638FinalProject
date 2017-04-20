415b231a56759560be14a7dca7040f4a44f3a0a6
Max Horowitz-Gelb <mhorowitzgelb@hotmail.com>
2017-04-19 18:33:36 -0700

    More work done o parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,40 @@
 import csv
+import sys
 import numpy as np
+from math import ceil
+from matplotlib import pyplot as plt
+time_step_size = 0.01
+
+
+def get_peak_intensities(all_times, all_intensities, start, end):
+    i = 0
+    time = 0
+    next = 0
+    offset = 0;
+    peak_intensities = []
+    peak_times = []
+    while all_times[i+1] < start:
+        i +=1
+    while next < end:
+        time = all_times[i]
+        if(i+1 >= len(all_times)):
+            break;
+        next = all_times[i +1]
+        time_width = next - time
+        j = 0
+        while True:
+            a = (offset + j * time_step_size)/ time_width
+            if( a >= 1 or time + a*time_width > end):
+                break
+            peak_intensities.append((1-a) * all_intensities[i] + a * all_intensities[i+1])
+            peak_times.append((1-a) * all_times[i] + a * all_times[i+1])
+            j += 1
+        offset = (a-1) * time_width
+        i += 1
+
+    return (peak_times,peak_intensities)
+
+
 
 def get_dataset(peak_range_file, intensities_file):
     dataset = {}
@@ -17,7 +52,8 @@ def get_dataset(peak_range_file, intensities_file):
             end_time = float(row[3])
             if not sequence in dataset:
                 dataset[sequence] = {}
-            dataset[sequence][file_name] = {}
+
+            dataset[sequence][file_name]= {}
             dataset[sequence][file_name]["start_time"] = start_time
             dataset[sequence][file_name]["end_time"] = end_time
 
@@ -31,18 +67,34 @@ def get_dataset(peak_range_file, intensities_file):
                 continue
             file_name = row[0]
             sequence = row[1]
+            charge = row[2]
+            ion = row[4]
             all_times = row[8]
             all_times = list(map(float, all_times.split(",")))
             all_intensities = row[9]
             all_intensities = list(map(float,all_intensities.split(',')))
 
-            intensities = get_peak_intensities(all_times, all_intensities,
+            (peak_times, peak_intensities) = get_peak_intensities(all_times, all_intensities,
                                                dataset[sequence][file_name]["start_time"], dataset[sequence][file_name]["end_time"])
+            if not charge in dataset[sequence][file_name]:
+                dataset[sequence][file_name][charge] = {}
+            dataset[sequence][file_name][charge][ion] = {}
+            dataset[sequence][file_name][charge][ion]['peak_intensities'] = peak_intensities
+            dataset[sequence][file_name][charge][ion]['peak_times'] = peak_times
+    return dataset
 
-            print(all_times)
-            counter += 1
-            if counter >= 1:
-                break
+import random
+dataset = get_dataset("data\smoothBoundaries.csv",'data\smoothCalibration.tsv')
 
+intensities = dataset[random.choice(list(dataset.items()))[0]]
 
-get_dataset("../data/ManualHannes.csv",'../data/ManualHannes.tsv')
+intensities = intensities[random.choice(list(intensities.items()))[0]]
+
+intensities = intensities[random.choice(list(intensities.items()))[0]]
+for a in intensities:
+    b = intensities[a]
+    c = b['peak_intensities']
+    times = b['peak_times']
+    plt.plot(times,c)
+plt.show()
+
